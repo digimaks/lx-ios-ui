8cf0afd46e45b0500997e2e5c7f26a525a2bf091
Unknovs <l22exc@gmail.com>
2026-09-24 09:12:48 +0300

    Add UIWrapperPackage scheme, and enhance WrapperView functionality

--- Sources/UIWrapperPackage/WrapperView.swift
+++ Sources/UIWrapperPackage/WrapperView.swift
@@ -16,6 +16,7 @@ import SnapKit
 
 public protocol WrapperMessageProtocol: Sendable {
     func messageReceived(interface: String, body: NSDictionary, from webView: WKWebView)
+    func openExternalUrl(url: URL)
 }
 
 @MainActor public class WrapperView: UIView, UIScrollViewDelegate {
@@ -28,8 +29,9 @@ public protocol WrapperMessageProtocol: Sendable {
     
     fileprivate var route: String = "dashboard"
     fileprivate var params: String = ""
-
-    public convenience init(resourcePath: String, interfaces: [String], route: String, params: String, color: UIColor) {
+    fileprivate var baseURL: String = ""
+    
+    public convenience init(resourcePath: String, interfaces: [String], route: String, params: String, color: UIColor, baseURL: String) {
         self.init()
         
         self.resourcePath = resourcePath
@@ -37,6 +39,7 @@ public protocol WrapperMessageProtocol: Sendable {
         self.route = route
         self.params = params
         self.color = color
+        self.baseURL = baseURL
         setUp()
     }
     
@@ -54,6 +57,7 @@ extension WrapperView {
     fileprivate func setupWebView() {
         webView = WKWebView(frame: .zero, configuration: self.getWKWebViewConfiguration())
         webView?.isOpaque = false
+        webView?.navigationDelegate = self
         webView?.scrollView.delegate = self
         webView?.backgroundColor = color
         if #available(iOS 16.4, *) {
@@ -65,10 +69,7 @@ extension WrapperView {
             self.addSubview(webView)
             
             webView.snp.makeConstraints { make in
-                make.top.equalTo(self.safeAreaLayoutGuide.snp.top)
-                make.left.equalTo(self.safeAreaLayoutGuide.snp.left)
-                make.right.equalTo(self.safeAreaLayoutGuide.snp.right)
-                make.bottom.equalTo(self.safeAreaLayoutGuide.snp.bottom)
+                make.edges.equalToSuperview()
             }
         }
     }
@@ -78,17 +79,16 @@ extension WrapperView {
     }
     
     private func getWKWebViewConfiguration() -> WKWebViewConfiguration {
-        //for logging
         let source = "function captureLog(msg) { window.webkit.messageHandlers.logHandler.postMessage(msg); } window.console.log = captureLog;"
         let source1 = "var meta = document.createElement('meta');" +
         "meta.name = 'viewport';" +
-        "meta.content = 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no';" +
+        "meta.content = 'width=device-width, initial-scale=1.0, user-scalable=no, viewport-fit=cover';" +
         "var head = document.getElementsByTagName('head')[0];" +
         "head.appendChild(meta);"
         
         let script = WKUserScript(source: source, injectionTime: .atDocumentEnd, forMainFrameOnly: false)
         let script1 = WKUserScript(source: source1, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-
+        
         
         let userController = WKUserContentController()
         userController.addUserScript(script)
@@ -115,8 +115,28 @@ extension WrapperView {
         configuration.preferences.javaScriptCanOpenWindowsAutomatically = true
         configuration.userContentController = userController
         configuration.preferences.setValue(true, forKey: "allowFileAccessFromFileURLs")
-
+        
         return configuration
+    }
+    
+    func getStatusBarHeight() -> CGFloat {
+        var statusBarHeight: CGFloat = 0
+        if #available(iOS 13.0, *) {
+            let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+            statusBarHeight = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        } else {
+            statusBarHeight = UIApplication.shared.statusBarFrame.height
+        }
+        return statusBarHeight + 4
+    }
+    
+    func getBottomInset() -> CGFloat {
+        if #available(iOS 13.0, *) {
+            let window = UIApplication.shared.windows.first { $0.isKeyWindow }
+            return window?.safeAreaInsets.bottom ?? 0
+        } else {
+            return UIApplication.shared.keyWindow?.safeAreaInsets.bottom ?? 0
+        }
     }
     
     @MainActor fileprivate func loadLocalHTML() {
@@ -136,11 +156,41 @@ extension WrapperView {
     }
 }
 
-extension WrapperView: WKScriptMessageHandler {
+extension WrapperView: WKScriptMessageHandler, WKNavigationDelegate {
     public func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
         let body = message.body
         guard let dictionary = body as? NSDictionary, let webView = webView else { return }
         self.delegate?.messageReceived(interface: message.name, body: dictionary, from: webView)
+    }
+    
+    public func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        print("WEBVIEW: \(self.webView)")
+        let height = self.getStatusBarHeight()
+        let bottomInset = self.getBottomInset()
+        self.webView?.evaluateJavaScript("document.documentElement.style.setProperty('--native-inset-top', '\(height)px');")
+        self.webView?.evaluateJavaScript("document.documentElement.style.setProperty('--native-inset-bottom', '\(bottomInset)px');")
+    }
+    
+    public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping @MainActor (WKNavigationActionPolicy) -> Void) {
+        
+        guard let url = navigationAction.request.url else {
+            decisionHandler(.allow)
+            return
+        }
+
+        let isUserGesture = navigationAction.navigationType == .linkActivated
+        
+        let absolute = url.absoluteString
+        
+        if !baseURL.isEmpty && (absolute.hasPrefix(self.baseURL) || absolute.hasPrefix("https://digimaks.eu")) {
+            let modified = url.absoluteString
+            guard let modifiedUrl = URL(string: modified) else { return }
+            self.delegate?.openExternalUrl(url: modifiedUrl)
+            decisionHandler(.cancel)
+            return
+        }
+        
+        decisionHandler(.allow)
     }
 }
 
